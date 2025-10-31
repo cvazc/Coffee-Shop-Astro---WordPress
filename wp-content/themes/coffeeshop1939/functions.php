79a18bb62dbda0780b3dab858f74010ac9725d89
Sebastián Contreras <contreras.sebastian.dev@gmail.com>
2025-10-31 12:58:32 -0600

    feat: Implement REST API support for featured images in pages

--- wp-content/themes/coffeeshop1939/functions.php
+++ wp-content/themes/coffeeshop1939/functions.php
@@ -5,3 +5,17 @@ function coffee_shop_setup() {
 }
 
 add_action('after_setup_theme', 'coffee_shop_setup');
+
+function coffee_shop_api_init() {
+    register_rest_field(
+        array('page'),
+        'featured_images',
+        array('get_callback' => 'get_featured_image')
+    );
+}
+
+add_action('rest_api_init','coffee_shop_api_init');
+
+function get_featured_image() {
+    return "Desde get_featured_image";
+}
