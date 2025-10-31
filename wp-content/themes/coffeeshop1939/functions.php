04644622314c390fe7b796be94dabad55bd86f43
Sebastián Contreras <contreras.sebastian.dev@gmail.com>
2025-10-31 13:18:54 -0600

    fix: Correct get_featured_image function to handle featured media and return image sizes

--- wp-content/themes/coffeeshop1939/functions.php
+++ wp-content/themes/coffeeshop1939/functions.php
@@ -14,8 +14,29 @@ function coffee_shop_api_init() {
     );
 }
 
-add_action('rest_api_init','coffee_shop_api_init');
+add_action('rest_api_init', 'coffee_shop_api_init');
 
-function get_featured_image() {
-    return "Desde get_featured_image";
+function get_featured_image($post) {
+    if (!$post['featured_media']) {
+        return false;
+    }
+
+    $image_sizes = get_intermediate_image_sizes();
+
+    $images = array();
+
+    foreach ($image_sizes as $size) {
+        if ($size === "2048x2048")
+            continue;
+
+        $image = wp_get_attachment_image_src($post['featured_media'], $size);
+
+        $images[$size === '1536x1536' ? 'full' : $size] = array(
+            'url' => $image[0],
+            'width' => $image[1],
+            'height' => $image[2]
+        );
+    }
+
+    return $images;
 }
