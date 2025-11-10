378b8f84fa87c4d0acb941c589f310fa939ccb79
Sebastian Contreras <contreras.sebastian.dev@gmail.com>
2025-11-10 18:12:13 +0000

    feat: Migrate repository structure to Docker environment. Add .gitignore and volume configurations.

---
+++ themes/coffeeshop1939/functions.php
@@ -0,0 +1,67 @@
+<?php
+
+add_filter('acf/settings/rest_api_format', function () {
+    return "standard";
+});
+
+function coffee_shop_setup() {
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'coffee_shop_setup');
+
+function coffee_shop_api_init() {
+    register_rest_field(
+        array('page', 'post'),
+        'featured_images',
+        array('get_callback' => 'get_featured_image')
+    );
+
+    register_rest_field(
+        array('post'),
+        'category_details',
+        array('get_callback' => 'get_post_categories')
+    );
+}
+
+add_action('rest_api_init', 'coffee_shop_api_init');
+
+function get_featured_image($post) {
+    if (!$post['featured_media']) {
+        return false;
+    }
+
+    $image_sizes = get_intermediate_image_sizes();
+
+    $images = array();
+
+    foreach ($image_sizes as $size) {
+        if ($size === "2048x2048")
+            continue;
+
+        $image = wp_get_attachment_image_src($post['featured_media'], $size);
+
+        $images[$size === '1536x1536' ? 'full' : $size] = array(
+            'url' => $image[0],
+            'width' => $image[1],
+            'height' => $image[2]
+        );
+    }
+
+    return $images;
+}
+
+function get_post_categories($post) {
+
+    return array_map(
+        function ($categoryId) {
+            $category = get_category($categoryId, 'ARRAY_A');
+
+            return [
+                'name' => $category['name'],
+                'slug' => $category['slug']
+            ];
+        },
+        $post['categories']
+    );
+}
